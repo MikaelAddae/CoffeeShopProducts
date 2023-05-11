8bd966accfb9963c4f02560812030531a64c5741
Mikael Addae <mikael.addae@gmail.com>
2023-05-11 17:12:53 -0400

    Products

---
+++ CoffeeShopRegistration/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly CoffeeDbContext _coffeeDbContext;
+
+        public HomeController(ILogger<HomeController> logger, CoffeeDbContext context)
+        {
+            _logger = logger;
+            _coffeeDbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        { 
+            return View();
+        }
+
+        public IActionResult Success(User result)
+        {
+            if(result.Password.ToLower().Contains("password"))
+            {
+                return RedirectToAction("Failed");
+            }
+            else
+            {
+                return View(result);
+            }
+        }
+
+        public IActionResult Failed()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopRegistration/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly CoffeeDbContext _coffeeDbContext;
+
+        public ProductController (CoffeeDbContext context)
+        {
+            _coffeeDbContext = context;
+        }
+        public IActionResult Index()
+        {
+            List<Product> offerings = _coffeeDbContext.Products.ToList();
+            return View(offerings);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Product p= _coffeeDbContext.Products.FirstOrDefault(p=>p.Id == id);
+
+            return View(p);
+        }
+    }
+}
+
+            //Product p = _coffeeDbContext.Products.FirstOrDefault(p => p.Id == item);
+            //_coffeeDBContext.Products.FirstOrDefault(e => e.Id == id);
+            //_employeeDbContext.Employees.Remove(e);
+            //_employeeDbContext.SaveChanges();
+            //return RedirectToAction("EmployeesView");
